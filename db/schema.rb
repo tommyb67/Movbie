a8e0f1a2efdebf51c2b92f20c97ae579f2cb3e8e
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-10 19:13:31 -0400

    Created actor model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710223003) do
+ActiveRecord::Schema.define(:version => 20130710231258) do
+
+  create_table "actors", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "movies", :force => true do |t|
     t.string   "title"
