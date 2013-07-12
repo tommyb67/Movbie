d7863abd247a244352e7abf489fd2d32237bf21f
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-12 10:25:12 -0400

    Refactored vote up and down buttons to helper methods

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,13 @@
 module MoviesHelper
+  def vote_up_button
+    unless @movie.rating > 99
+      link_to "<i class='icon-plus-sign'></i>".html_safe, voteup_path, method: 'post'
+    end
+  end
+
+  def vote_down_button
+    unless @movie.rating < 1
+      link_to "<i class='icon-minus-sign'></i>".html_safe, votedown_path, method: 'post'
+    end
+  end
 end
